037ea3b3992524c752e73815cfc1bb7f1ff11597
monotypeee <spair0039@gmail.com>
2023-01-10 09:51:07 +0900

    ✅⋅배열에 투두 추가

--- src/todo.js
+++ src/todo.js
@@ -2,16 +2,23 @@ import { useState } from "react";
 
 function Todo(){
   const [tdInput,setTdInput] = useState("");
-  // const [tdList,setTdList] = useState([]);
+  const [tdList,setTdList] = useState([]);
 
   function inputChange(event){
     setTdInput(event.target.value);
+  }
+  function addtdl(){
+    setTdList(function(now){
+      return([tdInput,...now]);
+    });
+    setTdInput("");
+    console.log(tdList);
   }
 
   return (
     <div>
       <input value={tdInput} type="text" placeholder="WHATUGONNADO?" onChange={inputChange}/>
-      <button>추가</button>
+      <button onClick={addtdl}>추가</button>
     </div>
   );
 }
