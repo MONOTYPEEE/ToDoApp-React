59112bf6ef68955bbb003236a21ca9606034f011
monotypeee <spair0039@gmail.com>
2023-01-10 09:46:46 +0900

    ✅⋅change시 상태변경

--- src/todo.js
+++ src/todo.js
@@ -1,6 +1,17 @@
-function Todo() {
+import { useState } from "react";
+
+function Todo(){
+  const [tdInput,setTdInput] = useState("");
+  // const [tdList,setTdList] = useState([]);
+
+  function inputChange(event){
+    setTdInput(event.target.value);
+  }
+
   return (
     <div>
+      <input value={tdInput} type="text" placeholder="WHATUGONNADO?" onChange={inputChange}/>
+      <button>추가</button>
     </div>
   );
 }
